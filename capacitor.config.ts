9e0065a1f5cff57696f4636fedc8544e257300d5
Taylor <taylorlacortsuporte@gmail.com>
2023-02-24 09:11:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Meu_Primeiro_Projeto',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
